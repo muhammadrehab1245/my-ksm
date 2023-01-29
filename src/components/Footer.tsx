02187654b2c1cf20bdf62a89ce010ad7638bcdb5
Hung Vu <vunamhung@gmail.com>
2023-01-29 11:49:14 +0700

    :recycle: Refactoring code.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,5 @@
-export const Footer = () => {
-  return (
-    <footer className="sticky bottom-0 z-10 w-full bg-gray-100 py-3 text-center">
-      <div>&copy; 2022. RENNAISANCE INC. All Rights Reserved.</div>
-    </footer>
-  );
-};
+export const Footer = () => (
+  <footer className="sticky bottom-0 z-10 w-full bg-gray-100 py-3 text-center">
+    <div>&copy; 2022. RENNAISANCE INC. All Rights Reserved.</div>
+  </footer>
+);
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,11 @@
 import Link from 'next/link';
 
-export const Header = () => {
-  return (
-    <header className="bg-white">
-      <div className="container text-center">
-        <Link className="inline-block" href="/">
-          <img className="h-20" src="/images/logo.svg" alt="logo" />
-        </Link>
-      </div>
-    </header>
-  );
-};
+export const Header = () => (
+  <header className="bg-white">
+    <div className="container text-center">
+      <Link className="inline-block" href="/">
+        <img className="h-20" src="/images/logo.svg" alt="logo" />
+      </Link>
+    </div>
+  </header>
+);
