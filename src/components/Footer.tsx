464bd95d23035e27142c7f60e5a80e90c93f2f0a
Hung Vu <vunamhung@gmail.com>
2023-01-29 11:50:53 +0700

    :recycle: Refactoring code.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 export const Footer = () => (
-  <footer className="sticky bottom-0 z-10 w-full bg-gray-100 py-3 text-center">
+  <footer className="py-3 text-center">
     <div>&copy; 2022. RENNAISANCE INC. All Rights Reserved.</div>
   </footer>
 );
