aa3e6eb2d07fa8841f836969f70c32c9cc6a3d88
Hung Vu <vunamhung@gmail.com>
2023-03-14 09:59:58 +0700

    footer links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,33 @@
-export const Footer = () => (
-  <footer className="py-3 text-center">
-    <div>&copy; 2022. 特定非営利活動法人グランドワークこしみず</div>
-  </footer>
-);
+import Link from 'next/link';
+import { useTranslation } from 'next-i18next';
+import { IconExternalLink } from '@tabler/icons';
+
+export const Footer = () => {
+  const { t } = useTranslation();
+
+  return (
+    <footer className="container flex flex-wrap justify-between py-3">
+      <ul className="flex flex-wrap space-x-4">
+        <li>
+          <Link className="flex items-center hover:underline" href="/privacy-policy" target="_blank">
+            {t('privacyPolicy')}
+            <IconExternalLink className="ml-1 inline" size={20} stroke={1} />
+          </Link>
+        </li>
+        <li>
+          <Link className="flex items-center hover:underline" href="/terms-and-conditions" target="_blank">
+            {t('termsAndConditions')}
+            <IconExternalLink className="ml-1 inline" size={20} stroke={1} />
+          </Link>
+        </li>
+        <li>
+          <Link className="flex items-center hover:underline" href="https://watashino-koshimizu.jp/tokushohou" target="_blank">
+            {t('guideForSpecifiedCommercialTransactionsAct')}
+            <IconExternalLink className="ml-1 inline" size={20} stroke={1} />
+          </Link>
+        </li>
+      </ul>
+      <div>&copy; 2022. 特定非営利活動法人グランドワークこしみず</div>
+    </footer>
+  );
+};
