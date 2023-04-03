95fac4563113107e30c088a0929cb406d644082f
Hung Vu <vunamhung@gmail.com>
2023-04-03 13:38:42 +0700

    🌟 feat(common.json): add translation for "invalidCardExpiryDate" in Japanese.
    🐛 fix(PaymentForm.tsx): add validation for card expiry date to prevent invalid dates
    
    🎨 style(PaymentForm.tsx): refactor credit card form to use IMaskInput and add cvv and cardholder name fields

--- src/components/PaymentForm.tsx
+++ src/components/PaymentForm.tsx
@@ -2,13 +2,14 @@ import { FC } from 'react';
 import { useRouter } from 'next/router';
 import { useTranslation } from 'next-i18next';
 import clsx from 'clsx';
+import dayjs from 'dayjs';
 import { MaskedRange } from 'imask';
 import toast from 'react-hot-toast';
 import { object, string } from 'yup';
 import { IMaskInput } from 'react-imask';
 import { useDisclosure } from '@mantine/hooks';
 import { useForm, yupResolver } from '@mantine/form';
-import { Alert, Button, Input, Modal, Radio, TextInput } from '@mantine/core';
+import { Alert, Button, Input, Modal, Radio } from '@mantine/core';
 import { http, store } from '@/utilities';
 import { FiCheckCircle, FiChevronRight } from 'react-icons/fi';
 import TermsAndConditions from '@/pages/hotus/terms-and-conditions';
@@ -22,13 +23,22 @@ export const PaymentForm: FC<{ couponCode?: string }> = ({ couponCode }) => {
 
   const schema = object({
     paymentMethod: string().required(t('required')),
-    cardNumber: string().when('paymentMethod', { is: (pm: string) => pm === 'CARD', then: (schema) => schema.required(t('required')) }),
+    cardNumber: string().when('paymentMethod', {
+      is: (pm: string) => pm === 'CARD',
+      then: (schema) => schema.required(t('required')),
+    }),
     expiryDate: string().when('paymentMethod', {
       is: (pm: string) => pm === 'CARD',
       then: (schema) => schema.required(t('required')).min(4, t('required')),
     }),
-    cvv: string().when('paymentMethod', { is: (pm: string) => pm === 'CARD', then: (schema) => schema.required(t('required')) }),
-    cardholderName: string().when('paymentMethod', { is: (pm: string) => pm === 'CARD', then: (schema) => schema.required(t('required')) }),
+    cvv: string().when('paymentMethod', {
+      is: (pm: string) => pm === 'CARD',
+      then: (schema) => schema.required(t('required')).min(3, t('required')),
+    }),
+    cardholderName: string().when('paymentMethod', {
+      is: (pm: string) => pm === 'CARD',
+      then: (schema) => schema.required(t('required')),
+    }),
   });
 
   const {
@@ -48,7 +58,12 @@ export const PaymentForm: FC<{ couponCode?: string }> = ({ couponCode }) => {
     validate: yupResolver(schema),
   });
 
+  // @ts-ignore
   const onSubmit = handleSubmit((values) => {
+    if (values.paymentMethod === 'CARD' && dayjs(`20${values.expiryDate.substring(2)}-${values.expiryDate.substring(0, 2)}`).isBefore(new Date())) {
+      return toast.error(t('invalidCardExpiryDate'));
+    }
+
     const { paymentMethod, cardNumber, expiryDate, cvv, cardholderName } = values;
 
     const data = {
@@ -131,49 +146,56 @@ export const PaymentForm: FC<{ couponCode?: string }> = ({ couponCode }) => {
     <form className="space-y-4" onSubmit={onSubmit}>
       <h3 className="h5">{t('paymentInformation')}</h3>
       <Radio value="CARD" checked={values.paymentMethod === 'CARD'} label={t('creditCard')} onChange={register('paymentMethod').onChange} />
-      {values.paymentMethod === 'CARD' && (
-        <div className="ml-8 space-y-2">
-          <Input.Wrapper withAsterisk label={t('cardNumber')} error={register('cardNumber').error}>
+      <div className={clsx('ml-8 space-y-2', values.paymentMethod === 'CARD' ? 'block' : 'hidden')}>
+        <Input.Wrapper withAsterisk label={t('cardNumber')} error={register('cardNumber').error}>
+          <IMaskInput
+            // @ts-ignore
+            className="mantine-Input-input mantine-TextInput-input mantine-1j89rho"
+            mask="0000 0000 0000 0000"
+            unmask={true}
+            onAccept={(value: any) => setFieldValue('cardNumber', value)}
+            placeholder={t('cardNumberPlaceholder')}
+          />
+        </Input.Wrapper>
+        <div className="flex gap-2">
+          <Input.Wrapper withAsterisk label={t('expiryDate')} error={register('expiryDate').error}>
             <IMaskInput
               // @ts-ignore
               className="mantine-Input-input mantine-TextInput-input mantine-1j89rho"
-              mask="0000 0000 0000 0000"
+              mask="m/y"
+              blocks={{
+                m: { mask: MaskedRange, from: 1, to: 12 },
+                y: { mask: MaskedRange, from: 10, to: 99 },
+              }}
               unmask={true}
-              onAccept={(value: any) => setFieldValue('cardNumber', value)}
-              placeholder={t('cardNumberPlaceholder')}
+              onAccept={(value: any) => setFieldValue('expiryDate', value)}
+              placeholder={t('expiryDatePlaceholder')}
             />
           </Input.Wrapper>
-          <div className="flex gap-2">
-            <Input.Wrapper withAsterisk label={t('expiryDate')} error={register('expiryDate').error}>
-              <IMaskInput
-                // @ts-ignore
-                className="mantine-Input-input mantine-TextInput-input mantine-1j89rho"
-                mask="m/y"
-                blocks={{
-                  m: { mask: MaskedRange, from: 1, to: 12 },
-                  y: { mask: MaskedRange, from: 10, to: 99 },
-                }}
-                unmask={true}
-                onAccept={(value: any) => setFieldValue('expiryDate', value)}
-                placeholder={t('expiryDatePlaceholder')}
-              />
-            </Input.Wrapper>
-            <Input.Wrapper withAsterisk label={t('cvv')} error={register('cvv').error}>
-              <IMaskInput
-                // @ts-ignore
-                className="mantine-Input-input mantine-TextInput-input mantine-1j89rho"
-                mask={Number}
-                min={0}
-                max={9999}
-                unmask={true}
-                onAccept={(value: any) => setFieldValue('cvv', value)}
-                placeholder={t('cvvPlaceholder')}
-              />
-            </Input.Wrapper>
-          </div>
-          <TextInput withAsterisk label={t('cardholderName')} placeholder={t('cardholderNamePlaceholder')} {...register('cardholderName')} />
+          <Input.Wrapper withAsterisk label={t('cvv')} error={register('cvv').error}>
+            <IMaskInput
+              // @ts-ignore
+              className="mantine-Input-input mantine-TextInput-input mantine-1j89rho"
+              mask={Number}
+              min={0}
+              max={9999}
+              unmask={true}
+              onAccept={(value: any) => setFieldValue('cvv', value)}
+              placeholder={t('cvvPlaceholder')}
+            />
+          </Input.Wrapper>
         </div>
-      )}
+        <Input.Wrapper withAsterisk label={t('cardholderName')} error={register('cardholderName').error}>
+          <IMaskInput
+            // @ts-ignore
+            className="mantine-Input-input mantine-TextInput-input mantine-1j89rho"
+            mask={/^[A-Za-z]+$/}
+            unmask={true}
+            onAccept={(value: any) => setFieldValue('cardholderName', value)}
+            placeholder={t('cardholderNamePlaceholder')}
+          />
+        </Input.Wrapper>
+      </div>
       <Radio value="TRANSFER" checked={values.paymentMethod === 'TRANSFER'} label={t('bankTransfer')} onChange={register('paymentMethod').onChange} />
       <h3 className="h5">{t('termsAndConditions')}</h3>
       <button
