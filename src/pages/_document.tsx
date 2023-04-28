7cb7aa8909993bfc5ba6bf79af3993506454200a
Hung Vu <vunamhung@gmail.com>
2023-04-28 13:21:26 +0700

    🎨 style(_document.tsx): add Noto Sans JP font to the project 🎨 style(tailwind.config.js): change default font to Noto Sans JP

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -18,6 +18,9 @@ export default class _Document extends Document {
       <Html lang="en">
         <Head>
           <link rel="icon" href="/icons/favicon.ico" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
         </Head>
         <body className="bg-gray-100 font-body">
           <Main />
