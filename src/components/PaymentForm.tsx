650c5bfd9cf2e3ac484339195c3da6807f782392
Hung Vu <vunamhung@gmail.com>
2023-05-19 15:41:39 +0700

    🐛 fix(PaymentForm.tsx): update cvv input mask to accept only 1 to 4 digits

--- src/components/PaymentForm.tsx
+++ src/components/PaymentForm.tsx
@@ -166,7 +166,7 @@ export const PaymentForm: FC<{ couponCode?: string }> = ({ couponCode }) => {
                 'mantine-Input-input mantine-TextInput-input mantine-1j89rho',
                 register('cvv').error && 'border-red-500 placeholder-red-500',
               )}
-              mask={/^[0-9]+$/}
+              mask={/^[0-9]{1,4}$/}
               unmask={true}
               onAccept={(value: any) => setFieldValue('cvv', value)}
               placeholder={t('cvvPlaceholder')}
