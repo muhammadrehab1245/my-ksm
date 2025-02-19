fea113e19d205dee602c414b129e671f779cc1a9
mrehab <rehab123335@gmail.com>
2025-02-19 15:54:29 +0500

    redirect issue trying to resolve with 307

--- src/pages/api/callback.tsx
+++ src/pages/api/callback.tsx
@@ -2,5 +2,5 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 export default function Callback(req: NextApiRequest, res: NextApiResponse) {
   const { query, body } = req;
-  return res.redirect(`/callback?MD=${query.MD}&param=${body.param}&requestorTransId=${body.requestorTransId}&event=${body.event}`);
+  return res.redirect(307, `/callback?MD=${query.MD}&param=${body.param}&requestorTransId=${body.requestorTransId}&event=${body.event}`);
 }
